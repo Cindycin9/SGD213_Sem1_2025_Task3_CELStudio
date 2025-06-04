d9d01fed8c90581d81c2b081fd876e583a30a4ff
Cindycin9 <cae018@student.usc.edu.au>
2025-06-04 10:15:20 +1000

    MainMenu Implemented
    Start and Quit buttons working

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,6 +1,18 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class MainMenu : MonoBehaviour
 {
+    // Loads the main game scene
+    public void PlayGame()
+    {
+     SceneManager.LoadScene("breh");
+    }
 
+    // Quits the game
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 }
